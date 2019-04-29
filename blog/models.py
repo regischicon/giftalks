134355d089d1074eaa1ae8d3475f1f0f74172610
elgranchicon <regis_ch@yahoo.com>
2019-04-29 14:52:03 -0300

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -13,7 +13,7 @@ class Post(models.Model):
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    self.title = self.gifmaker
+    title = self.gifmaker
 
     def __str__(self):
         return self.title
