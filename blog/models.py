59817590e1cdfff67bdc76f473efe781e2a35665
elgranchicon <regis_ch@yahoo.com>
2019-04-29 11:13:01 -0300

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+import time
+import giphy_client
+from giphy_client.rest import ApiException
+from pprint import pprint
 
 
 class Post(models.Model):
@@ -15,3 +19,22 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('post-detail', kwargs={'pk': self.pk})
+    def gifmaker(self):
+        # Create your tests here.
+        ############API GIPHY
+        api_instance = giphy_client.DefaultApi()
+        api_key = 'U4A5T0UYFd2mIYANkPWOxBZjjuWvATAQ' # str | Giphy API Key.
+        tag = self.title # str | Filters results by specified tag. (optional)
+        rating = 'g' # str | Filters results by specified rating. (optional)
+        fmt = 'json' # str | Used to indicate the expected response format. Default is Json. (optional) (default to json)
+
+        #try:
+            # Random Endpoint
+        api_response = api_instance.gifs_random_get(api_key, tag=tag, rating=rating, fmt=fmt)
+            #pprint(api_response)
+        self.title = api_response
+        #except ApiException as e:
+            #print("Exception when calling DefaultApi->gifs_random_get: %s\n" % e)
+
+        return self.title
+        ############API GIPHY
